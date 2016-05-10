5b3e3acb208eb108ad7effa0afa41b4325ee10a3
josysk8 <joseph.devarenne@viacesi.fr>
2016-05-10 14:49:53 +0200

    research done

--- js/getMovie.js
+++ js/getMovie.js
@@ -19,6 +19,28 @@ function successGetPopular(data)
 	$("#mainWrapper").append(htmlPopular);
 };
 
+function successTopRated(data)
+{
+	var movieTopRated = JSON.parse(data);
+	var htmlTopRated = '<section id ="topRated"><h1>Meilleures notes</h1>';
+	for (var i = movieTopRated.length - 1; i >= 0; i--) {
+		htmlTopRated = htmlTopRated.concat(createElement(movieTopRated[i]));
+	};
+	htmlTopRated = htmlTopRated.concat('</section>');
+	$("#mainWrapper").append(htmlTopRated);
+};
+
+function successResearch(data)
+{
+	var movieSearch = JSON.parse(data);
+	var htmlSearch = '<section id ="research"><h1>Recherche</h1>';
+	for (var i = movieSearch.length - 1; i >= 0; i--) {
+		htmlSearch = htmlSearch.concat(createElement(movieSearch[i]));
+	};
+	htmlSearch = htmlSearch.concat('</section>');
+	$("#mainWrapper").append(htmlSearch);
+};
+
 function errorCB(data) {
 	console.log("Error callback: " + data);
 };
@@ -26,10 +48,14 @@ function errorCB(data) {
 function initPage()
 {
 	theMovieDb.movies.getNowPlaying({}, successGetNowPlaying, errorCB);
-	theMovieDb.movies.getPopular({}, successGetPopular, errorCB)
+	theMovieDb.movies.getPopular({}, successGetPopular, errorCB);
+	theMovieDb.movies.getTopRated({}, successTopRated, errorCB);
 };
 
 $(document).ready()
 {
 	initPage();
+	$("#research").on('input',function(e){
+		theMovieDb.search.getMovie({"query":$("#research").val()}, successResearch, errorCB)
+	});
 };
