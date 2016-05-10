031dcb472763b1f523e9df73fff45c6dcedf5c96
Guillaume Courtin <gui.courtin@gmail.com>
2016-05-10 11:00:22 +0200

    add layout.php add basic routing

--- index.php
+++ index.php
@@ -5,3 +5,17 @@
  * Date: 10/05/2016
  * Time: 10:03
  */
+
+$layout = "layout.php";
+
+$route = !empty($_REQUEST['route']) ? $_REQUEST['route'] : "index";
+
+$htmlContent = "<h1>TESTOUILLE</h1>";
+
+switch ($route)
+{
+	case "index":
+		break;
+}
+
+include $layout;
