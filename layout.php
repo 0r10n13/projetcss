1f023249e8821d6919142c20e35f3321370031ae
maxime <maxime.mutel@viacesi.fr>
2016-05-10 15:06:12 +0200

    couleur header

--- layout.php
+++ layout.php
@@ -19,7 +19,8 @@
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 <header>
-	<a href="index.php" id="logo">RIL</a>
+	<a href="index.php" id="logo">RILFLIX</a>
+	
 </header>
 <!-- Add your site or application content here -->
 <input type="text" id="research" name="research">
